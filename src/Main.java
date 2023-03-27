b26b37a4e08bb93db44cb8f0b02155500fa6b154
Yurii <shadowjura82@gmail.com>
2023-03-27 13:28:08 +0200

    task1 has been completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+
+    public static void task1() {
+        byte humanAge = 10;
+        if (humanAge >= 18) {
+            System.out.println("Если возраст человека равен " + humanAge + ", то он совершеннолетний");
+        } else {
+            System.out.println("Если возраст человека равен " + humanAge + ", то он не достиг совершеннолетия, нужно немного подождать");
+        }
     }
 }

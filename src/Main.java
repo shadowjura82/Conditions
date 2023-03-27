1eb61e36b18514ea3f4d1d273439632a3ce21dcd
Yurii <shadowjura82@gmail.com>
2023-03-27 13:34:52 +0200

    task2 has been completed

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
     public static void task1() {
@@ -9,6 +10,15 @@ public class Main {
             System.out.println("Если возраст человека равен " + humanAge + ", то он совершеннолетний");
         } else {
             System.out.println("Если возраст человека равен " + humanAge + ", то он не достиг совершеннолетия, нужно немного подождать");
+        }
+    }
+
+    public static void task2() {
+        byte temperatureOutside = 6;
+        if (temperatureOutside < 5) {
+            System.out.println("На улице " + temperatureOutside + " градусов, нужно надеть шапку");
+        } else {
+            System.out.println("На улице " + temperatureOutside + " градусов, можно идти без шапки");
         }
     }
 }
